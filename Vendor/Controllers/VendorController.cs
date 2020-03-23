6012a9f42abfe10bba44b30c2a437345dd67be88
acolistro <arcolistro@gmail.com>
2020-03-23 07:51:13 -0700

    add vendor CRUD functionality, move files around, refactor models to remove _count.

---
+++ Vendor/Controllers/VendorController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorOrders.Models;
+
+namespace VendorOrders.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description, string address)
+    {
+      Vendor newVendor = new Vendor(name, description, address);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("vendors/{id}")]
+    public ActionResult show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
